836a50298393eded7d58e438dc172e59e445a97f
seungyongI <lsy03108@gmail.com>
2024-07-16 14:57:53 +0900

    [level 0] Title: 문자열 겹쳐쓰기, Time: 0.29 ms, Memory: 76.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.java
@@ -0,0 +1,20 @@
+class Solution {
+    public String solution(String my_string, String overwrite_string, int s) {
+        String answer = "";
+        char[] myChar = my_string.toCharArray();
+        char[] overChar = overwrite_string.toCharArray();
+        char[] result = answer.toCharArray();
+
+        for (int i = 0; i < overChar.length; i++) {
+            myChar[s] = overChar[i];
+            s++;
+        }
+        my_string = new String(myChar);
+        answer = my_string;
+        System.out.println(answer);
+        return answer;
+        
+        
+    }
+
+}
