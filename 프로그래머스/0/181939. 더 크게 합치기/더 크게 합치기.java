17bfcafa8296ee752aa9d39e62bb0659a57a03d5
seungyongI <lsy03108@gmail.com>
2024-07-17 16:53:16 +0900

    [level 0] Title: 더 크게 합치기, Time: 2.00 ms, Memory: 77.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int solution(int a, int b) {
+        int answer = 0;
+        
+        int answer1 = Integer.parseInt(Integer.toString(a) + Integer.toString(b));
+        int answer2 = Integer.parseInt(Integer.toString(b) + Integer.toString(a));
+        if (answer1 >= answer2) {
+            answer = answer1;
+        } else {
+            answer = answer2;
+        }
+        
+        return answer;
+    }
+}
