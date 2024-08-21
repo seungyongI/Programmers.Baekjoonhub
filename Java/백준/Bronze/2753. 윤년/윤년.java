6c5361c8e28d19b6b52e78e330b31b0e1b821608
seungyongI <lsy03108@gmail.com>
2024-08-21 10:07:53 +0900

    [Bronze V] Title: 윤년, Time: 164 ms, Memory: 17760 KB -BaekjoonHub

---
+++ Java/백준/Bronze/2753. 윤년/윤년.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int y = scan.nextInt();
+        
+        if((y%4==0 && y%100!=0) || y % 400 == 0){
+            System.out.println(1);
+        } else {
+            System.out.println(0);
+        }
+    }
+}
