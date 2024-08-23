8d17dfd5c39b6190fa5b94ed1b9c46c74fb5f11a
seungyongI <lsy03108@gmail.com>
2024-08-23 16:13:24 +0900

    [Bronze V] Title: 개수 세기, Time: 104 ms, Memory: 14384 KB -BaekjoonHub

---
+++ Java/백준/Bronze/10807. 개수 세기/개수 세기.java
@@ -0,0 +1,28 @@
+import java.io.*;
+
+public class Main{
+    public static void main(String[] args) throws IOException{
+        BufferedReader br
+                = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw
+                = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n = Integer.parseInt(br.readLine());
+        String[] arr = br.readLine().split(" ");
+        int v = Integer.parseInt(br.readLine());
+        int cnt = 0;
+        int num = 0;
+
+        for(int i = 0; i < n; i++) {
+            num = Integer.parseInt(arr[i]);
+
+            if(v == num) {
+                cnt++;
+            }
+        }
+        bw.write(""+cnt);
+        
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
